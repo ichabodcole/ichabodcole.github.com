d9c5a4cda78a380e48384e028158c63094b99a29
ichabodcole <ichabodcole@gmail.com>
2013-12-05 13:54:41 -0800

    Refactor Scrollemetry class

--- app/js/scrollemetry.js
+++ app/js/scrollemetry.js
@@ -1,34 +1,52 @@
-var ichaBlog = (function(app) {
+var Scrollemetry = (function() {
+  /*
+  * Scrollemetry © Cole Reed 2013
+  * v0.1.0
+  */
 
-  function Scrollemetry () {
-    this.lastScroll = 0;
-    this.direction  = null;
-    this.speed      = 0;
-    this.scroll     = 0;
-    this.markers    = [];
+  // START CUSTOMEVENT POLYFILL
+  /***************************************/
+  // This is a polyfill to add the CustomEvent DOM API to ie 9 & 10
+  // For more info and the original code see the below link.
+  // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   };
+
+  // This "try" keeps PhantomJs from zeroing out tests.
+  try {
+    CustomEvent.prototype = window.CustomEvent.prototype;
+  } catch (err) {
+    console.log('Your browser does not support the CustomEvent API', err);
+  }
+  window.CustomEvent = CustomEvent;
+  /***************************************/
+  // END CUSTOMEVENT POLYFILL
+
+  function Scrollemetry (el) {
+    this.el          = el;
+    this._lastScroll = 0;
+    this._direction  = null;
+    this._speed      = 0;
+    this._scroll     = 0;
+    this._markers    = [];
   }
 
   // Scrollemetry.prototype.
-
   Scrollemetry.prototype = function (){
 
-    var addMarker = function (id, scrollPosition) {
-      var index, marker;
-      // Create a marker object
-      marker = { id:id, scrollPosition:scrollPosition, past:false };
-      // markerExists is a private method,
-      // must use the call method to pass the correct "this" scope
-      index = markerExists.call(this, id);
-
-      if(index === void(0)) {
-        this.markers.push(marker);
-      }else {
-        this.markers[index] = marker;
-      }
+    // Private functions
+    var _dispatchEvent = function (marker){
+      // CustomEvent is a DOM method
+      var e = new CustomEvent(marker.id, {detail:marker});
+      this.el.dispatchEvent(e);
     }
 
-    , markerExists = function (id) {
-      var index = this.markers.map(function(element, index){
+    , _markerExists = function (id) {
+      var index = this._markers.map(function(element, index){
         if(element.id === id) {
           return index;
         }
@@ -36,45 +54,91 @@ var ichaBlog = (function(app) {
       return index[0];
     }
 
+    , _updateMarkers = function () {
+      var _self = this;
+      this._markers.forEach(function(element, index, array) {
+        if(_self._scroll >= element.scrollPosition && element.past === false) {
+          element.past = true;
+          // set the correct scope for the private dispatchEvent function.
+          _dispatchEvent.call(_self, element);
+        } else if (_self._scroll < element.scrollPosition && element.past === true){
+          element.past = false;
+          _dispatchEvent.call(_self, element);
+        }
+      });
+    }
+
+    // API methods
+    , addMarker = function (id, scrollPosition) {
+      var index, marker;
+      // Create a marker object
+      marker = { id:id, scrollPosition:scrollPosition, past:false };
+      // markerExists is a private method,
+      // must use the call method to pass the correct "this" scope
+      index = _markerExists.call(this, id);
+
+      if(index === void(0)) {
+        this._markers.push(marker);
+      }else {
+        this._markers[index] = marker;
+      }
+    }
+
     , getMarkerById = function (id) {
-      var marker = this.markers.filter(function(element) {
+      var marker = this._markers.filter(function(element) {
         return element.id === id;
       });
       return marker[0];
     }
 
-    , updateMarkersPastProp = function () {
-    //   this.markers.forEach(function(element, index, array) {
-    //     if(this.scroll > element.position) {
-    //       element.past = true;
-    //     } else {
-    //       element.past = false;
-    //     }
-    //   });
+    , update = function (scroll) {
+      this._scroll = scroll;
+      this._speed  = this._scroll - this._lastScroll;
+
+      if(this._scroll > this._lastScroll){
+        this._direction = 'down';
+      } else if (this._scroll < this._lastScroll){
+        this._direction = 'up'
+      } else {
+        this._direction = null;
+      }
+      // Set the correct this context
+      _updateMarkers.call(this);
+      this._lastScroll = this._scroll;
     }
 
-    , update = function () {
-    //   this.scroll = document.body.scrollTop;
-    //   this.updateMarkers();
+    // Getters
+    , speed = function () {
+      return this._speed;
+    }
 
-    //   if (this.scroll > this.lastScroll) {
-    //     this.direction = 'down';
-    //   } else {
-    //     this.direction = 'up';
-    //   }
+    , direction = function () {
+      return this._direction;
+    }
 
-    //   this.speed = this.scroll - this.lastScroll;
-    //   this.lastScroll = this.scroll;
+    , scroll = function () {
+      return this._scroll;
+    }
+
+    , markers = function () {
+      return this._markers;
+    }
+
+    , lastScroll = function () {
+      return this._lastScroll;
     }
 
     return {
+      speed: speed,
+      direction: direction,
+      scroll: scroll,
+      markers: markers,
+      lastScroll: lastScroll,
       addMarker: addMarker,
       getMarkerById: getMarkerById,
       update: update
     }
   }();
 
-  app.scroller = Scrollemetry;
-  return app;
-
-})(ichaBlog || {});
+  return Scrollemetry;
+})();
